3ade52b130caee0deb6107ffc44105ce299969c5
Kevin McGrath <kmcgrath@cpan.org>
2006-12-08 15:10:53 +0000

    Import of KMCGRATH/Catalyst-Plugin-AjaxTags-0.03 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-AjaxTags
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         KMCGRATH/Catalyst-Plugin-AjaxTags-0.03.tar.gz
    gitpan-cpan-author:       KMCGRATH
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/AjaxTags.pm
+++ lib/Catalyst/Plugin/AjaxTags.pm
@@ -1,10 +1,30 @@
+=head1 NAME
+
+Catalyst::Plugin::AjaxTags - AjaxTags plugin for Catalyst
+
+=head1 SYNOPSIS
+
+  Add AjaxTags to the list of imported moduled in Catalyst.
+
+  use Catalyst qw/-Debug
+                HTML::AjaxTags /;
+
+
+=head1 DESCRIPTION
+
+Use AjaxTags as a plugin for Catalyst
+
+=cut
+
+
+
 package Catalyst::Plugin::AjaxTags;
 
 use strict;
 use warnings;
 use base 'Class::Data::Inheritable';
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 __PACKAGE__->mk_classdata('ajaxtags');
 
